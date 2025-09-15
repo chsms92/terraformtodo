eafa61b3b24e8c4d90fbbeb6685872c696ddc221
Chandra Pratap Singh <chandrapratap.smsit@gmail.com>
2025-09-15 23:10:02 +0530

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-devopsinsiders"
-    storage_account_name = "twostates"
+    resource_group_name  = "chandra-rg"
+    storage_account_name = "chandrastorage786"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
